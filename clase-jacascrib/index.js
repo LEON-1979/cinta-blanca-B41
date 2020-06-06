302c6f71f8baa7ba0ac21501362a86c24c6ed867
leon <leonmesa1979@hotmail.com>
2020-06-05 21:15:51 -0500

    variable 1 clase de JS

---
+++ clase-jacascrib/index.js
@@ -0,0 +1,28 @@
+//variable
+
+//string (cadena de texto)
+var limon="un limon";
+
+//number
+var azucar=20;
+
+
+//booleanos (solo puede tener verdadero(true) o falso(false))
+var tieneDulce=false
+
+//arreglos
+var instrumentos=["cuchillo", "tabla para cortar", "vaso", "cuchara", "esprimidor"];
+
+console.log(instrumentos[2])
+console.log(limon)
+
+//objetos
+var limonero={
+    nombre: "leon",//atributos o propiedad
+    edad: 40,//atributos o propiedad
+    estatura: "170cm",//atributos o propiedad
+}
+console.log(limonero.nombre + " tiene una estatura " + limonero.estatura)//concatenar strings (unir cadena de texto)
+
+var nombre = prompt("escribe tu nombre")
+console.log(nombre)
